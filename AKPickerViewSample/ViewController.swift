4a16b72f999d85681e9b1053a4f294a1c0cc3bd6
Acedev1 <jpapphouse@gmail.com>
2015-03-23 20:21:56 +0100

    Implicitly unwrapped optional bug fux.

--- AKPickerViewSample/ViewController.swift
+++ AKPickerViewSample/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController, AKPickerViewDataSource, AKPickerViewDele
 	uncomment '-pickerView:imageForItem:' to see how it works.
 
 	*/
-	func pickerView(pickerView: AKPickerView, titleForItem item: Int) -> String {
+	func pickerView(pickerView: AKPickerView, titleForItem item: Int) -> NSString {
 		return self.titles[item]
 	}
 
